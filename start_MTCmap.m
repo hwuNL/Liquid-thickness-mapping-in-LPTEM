2485fa4a34e6fbdf994a79053c0ee94294a2dea7
Hanglong Wu <79865709+hwuNL@users.noreply.github.com>
2022-03-11 10:59:12 +0100

    Update start_MTCmap.m

--- start_MTCmap.m
+++ start_MTCmap.m
@@ -2,7 +2,7 @@
 %The filename of the SER file of the LP-TEM image with the viewing window
 fname = '550x_4s_1.ser';
 %The filename of the SER file of the flat field image without sample
-ffd = '5k_ffd_1.ser';
+ffd = '550x_ffd_1.ser';
 %The exposure time of the LP-TEM image containging the viewing window, note that here the default exposure time of the flat field image is 1 s;
 t = 4 % s;
 %The thickness of the single-layer silicon nitride window 
